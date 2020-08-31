fa42437a3385c65f02f9b9fa5cabd6d1775f3718
Renê <renejr.arraes286@gmail.com>
2020-08-31 16:00:19 -0300

    adding the slider

--- lib/main.dart
+++ lib/main.dart
@@ -17,39 +17,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double _currentSliderValue = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Currency Converter"),
+      appBar: AppBar(
+        title: Text("Currency Converter"),
+      ),
+      body: Column(children: <Widget>[
+        Slider(
+          value: _currentSliderValue,
+          min: 1,
+          max: 10,
+          divisions: 10,
+          label: _currentSliderValue.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _currentSliderValue = value;
+            });
+          },
         ),
-        body: Center(
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.attach_money),
-                  title: Text('€ EURO'),
-                  subtitle: Text('Conversão de Reais para Euro.'),
-                ),
-                ButtonBar(
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text("R\$ REAIS"),
-                      textColor: Colors.grey,
-                      onPressed: () {/* ... */},
-                    ),
-                    Icon(Icons.arrow_right),
-                    FlatButton(
-                      child: Text('€ EURO'),
-                      onPressed: () {/* ... */},
-                    ),
-                  ],
-                ),
-              ],
-            ),
+        Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.attach_money),
+                title: Text('€ EURO'),
+                subtitle: Text('Conversão de Reais para Euro.'),
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("R\$ REAIS"),
+                    textColor: Colors.grey,
+                    onPressed: () {/* ... */},
+                  ),
+                  Icon(Icons.arrow_right),
+                  FlatButton(
+                    child: Text('€ EURO'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ]),
+    );
   }
 }
