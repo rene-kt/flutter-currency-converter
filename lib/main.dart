a06c17ce6be22b6693a0181f6a6d57f244f7bdde
Renê <renejr.arraes286@gmail.com>
2020-09-01 09:20:02 -0300

    creating other cards

--- lib/main.dart
+++ lib/main.dart
@@ -22,49 +22,138 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Currency Converter"),
-      ),
-      body: Column(children: <Widget>[
-        Slider(
-          value: _currentSliderValue,
-          min: 1,
-          max: 10,
-          divisions: 10,
-          label: _currentSliderValue.round().toString(),
-          onChanged: (double value) {
-            setState(() {
-              _currentSliderValue = value;
-            });
-          },
+        appBar: AppBar(
+          title: Text("Currency Converter"),
+          backgroundColor: Colors.deepPurple,
         ),
-        Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.attach_money),
-                title: Text('€ EURO'),
-                subtitle: Text('Conversão de Reais para Euro.'),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Slider(
+              value: _currentSliderValue,
+              activeColor: Colors.deepPurple,
+              inactiveColor: Colors.deepPurple,
+              min: 0,
+              max: 10,
+              divisions: 10,
+              label: _currentSliderValue.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _currentSliderValue = value;
+                });
+              },
+            ),
+            Card(
+                child: AnimatedContainer(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
               ),
-              ButtonBar(
+              duration: Duration(seconds: 1),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  FlatButton(
-                    child: Text("R\$ REAIS"),
-                    textColor: Colors.grey,
-                    onPressed: () {/* ... */},
+                  ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text(
+                      '€ EURO',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                    subtitle: Text('Conversão de Reais para Euro.'),
                   ),
-                  Icon(Icons.arrow_right),
-                  FlatButton(
-                    child: Text('€ EURO'),
-                    onPressed: () {/* ... */},
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text("R\$ $_currentSliderValue.00"),
+                        textColor: Colors.grey,
+                        onPressed: () {/* ... */},
+                      ),
+                      Icon(Icons.arrow_right),
+                      FlatButton(
+                        child: Text(
+                          '€ EURO',
+                          style: TextStyle(color: Colors.deepPurple),
+                        ),
+                        onPressed: () {/* ... */},
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
-      ]),
-    );
+            )),
+            Card(
+                child: AnimatedContainer(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              duration: Duration(seconds: 1),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text(
+                      '£ LIBRA',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                    subtitle: Text('Conversão de Reais para Libra.'),
+                  ),
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text("R\$ $_currentSliderValue.00"),
+                        textColor: Colors.grey,
+                        onPressed: () {/* ... */},
+                      ),
+                      Icon(Icons.arrow_right),
+                      FlatButton(
+                        child: Text(
+                          '£ LIBRA',
+                          style: TextStyle(color: Colors.deepPurple),
+                        ),
+                        onPressed: () {/* ... */},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
+            Card(
+                child: AnimatedContainer(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              duration: Duration(seconds: 1),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.attach_money),
+                    title: Text(
+                      '\$ DOLAR',
+                      style: TextStyle(color: Colors.deepPurple),
+                    ),
+                    subtitle: Text('Conversão de Reais para Dolar.'),
+                  ),
+                  ButtonBar(
+                    children: <Widget>[
+                      FlatButton(
+                        child: Text("R\$ $_currentSliderValue.00"),
+                        textColor: Colors.grey,
+                        onPressed: () {/* ... */},
+                      ),
+                      Icon(Icons.arrow_right),
+                      FlatButton(
+                        child: Text(
+                          '\$ DOLAR',
+                          style: TextStyle(color: Colors.deepPurple),
+                        ),
+                        onPressed: () {/* ... */},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
+          ]),
+        ));
   }
 }
